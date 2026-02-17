9583645eb9308ea26f95cf39e00a762361a1e454
Raushan Kumar <er_raushan@outlook.com>
2026-02-18 01:22:13 +0530

    Delete project directory

--- project/js.js
+++
@@ -1,6 +0,0 @@
-function order(productName) {
-    const phoneNumber = "91XXXXXXXXXX"; // your WhatsApp number
-    const message = `Hello, I want to order: ${productName}`;
-    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    window.open(url, "_blank");
-}
